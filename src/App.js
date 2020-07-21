636cef65d3e1da9635f1ca47de292163a81149e9
Adam Smith <coloradoasmith@gmail.com>
2020-07-20 20:54:40 -0600

    basic api functioning

--- src/App.js
+++ src/App.js
@@ -5,6 +5,42 @@ import SortBook from "./components/sortBook/sortBook.js";
 import BookList from "./components/bookList/bookList";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+    };
+  }
+
+  componentDidMount() {
+    let params = {
+      q: "Henry III",
+      maxResults: 5,
+      key: "AIzaSyBUK42prIOR_hPyH0kdoJqTm8dq7R1WW6E",
+    };
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${params.q}&maxResults=5&key=${params.key}`;
+
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Something went wrong, please try again later.");
+        }
+        return res;
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          books: data,
+          error: null,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          error: err.message,
+        });
+      });
+  }
+
   render() {
     return (
       <main className="App">
@@ -12,7 +48,7 @@ class App extends Component {
 
         <SearchBook />
         <SortBook />
-        <BookList />
+        <BookList books={this.state.books} />
       </main>
     );
   }
